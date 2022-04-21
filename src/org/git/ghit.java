32d76cb368f7c86919444f3ae077a7d2dce5e611
EniyavanThangaraj <eniyavant405@gmail.com>
2022-04-21 11:52:45 +0530

    yruskk

--- src/org/git/ghit.java
+++ src/org/git/ghit.java
@@ -2,8 +2,8 @@ package org.git;
 
 public class ghit {
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
+		System.out.println("test76");
+		System.out.println("test98");
 	}
 
 }
